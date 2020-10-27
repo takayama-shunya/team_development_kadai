eed2a351a2633fa50c8cf0c300bb178a41952536
takayama shunya <katonbo1@icloud.com>
2020-10-27 20:21:48 +0900

    ロボコップ自動修正

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,6 +1,6 @@
 class AgendasController < ApplicationController
-  before_action :set_agenda, only: %i[ destroy ]
-  before_action :admin_agenda_destroy, only: %i[ destroy ]
+  before_action :set_agenda, only: %i[destroy]
+  before_action :admin_agenda_destroy, only: %i[destroy]
 
   def index
     @agendas = Agenda.all
@@ -16,7 +16,7 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
     end
