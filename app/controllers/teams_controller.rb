f02141e5860db023f1736e338e7e2f245daf5859
takayama shunya <katonbo1@icloud.com>
2020-11-01 18:30:19 +0900

    チーム編集制限追加

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
+  before_action :set_team_owner, only: %i[edit]
 
   def index
     @teams = Team.all
@@ -55,5 +56,9 @@ class TeamsController < ApplicationController
 
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
+  end
+
+  def set_team_owner
+    redirect_to @team, notice: "チームリーダー以外は編集できません" if current_user.email != @team.owner.email
   end
 end
